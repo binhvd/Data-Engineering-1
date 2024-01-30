86ed01c53bdaecb09a174719e630fbd0fe8fe096
Binh Vu <vuducbinh2001@yahoo.com>
2024-01-30 21:42:51 +0100

    Update entrypoint.sh

--- spark-cluster/config/entrypoint.sh
+++ spark-cluster/config/entrypoint.sh
@@ -5,10 +5,5 @@ set -e
 # Start services in the background
 service ssh start
 
-if [[ $TAILSCALE_KEY ]]; then
-    tailscale up --authkey ${TAILSCALE_KEY}
-    echo "Tailscale started..."
-fi
-
 # Do nothing and just keep the container alive
 sleep infinity
