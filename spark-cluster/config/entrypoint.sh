83bcb79b21f30d2acdafa22eca24bb22fb8b41b6
Binh Vu <bvu@ftk.de>
2024-02-02 16:53:55 +0100

    Update configuration

--- spark-cluster/config/entrypoint.sh
+++ spark-cluster/config/entrypoint.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+# Change dns config
+HDFS_SITE="/usr/local/hadoop/etc/hadoop/hdfs-site.xml"
+NETWORK="tailscale0"
+
+if [ -d "/sys/class/net/$NETWORK" ]; then
+	sed -i "/dfs.datanode.dns.interface/{n;s/default/$NETWORK/}" $HDFS_SITE
+fi
+
 # Start services in the background
 service ssh start
 
